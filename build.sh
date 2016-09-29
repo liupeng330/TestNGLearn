a68243316f7c2877e6e112ddf9c3ba786a8f3933
liupeng330 <330liupeng@gmail.com>
2016-09-29 22:47:52 +0800

    6

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 mvn clean test -Denv.OS=machahah -Denv.USER=aaabbb -Dgroups=Sniff
+mvn clean test -Denv.OS=machahah -Denv.USER=aaa -DsuiteFile=testng1.xml
+mvn clean test -Denv.OS=machahah -Denv.USER=aaa -DsuiteFile=testng2.xml
